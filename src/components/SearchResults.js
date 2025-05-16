09e254477cbafe7e291b228c2548d7cf4e29ef4f
Nitin Reddy Kamsani <kamsaninitinreddy@gmail.com>
2025-05-16 13:22:35 +0530

    built search results page

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 import { Api_key } from '../utils/constants';
 
 const YOUTUBE_API = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=";
@@ -12,10 +12,17 @@ const SearchResults = () => {
   const query = useQuery().get("q");
   const [videos, setVideos] = useState([]);
 
+  const navigate = useNavigate();
+
+  function handleNavigate(videoId){
+    navigate(`/watch?v=${videoId}`);
+  }
+
   useEffect(() => {
     async function fetchVideos() {
       const res = await fetch(`${YOUTUBE_API}${encodeURIComponent(query)}&key=${Api_key}`);
       const json = await res.json();
+      console.log(json);
       setVideos(json.items || []);
     }
 
@@ -26,9 +33,10 @@ const SearchResults = () => {
     <div className="p-4">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {videos.map(video => (
-          <div key={video.id.videoId || video.etag} className="border p-2 rounded shadow">
+          <div key={video.id.videoId || video.etag} className="border p-2 rounded shadow" onClick={()=>handleNavigate(video.id.videoId)}>
             <img src={video.snippet.thumbnails.medium.url} alt="thumbnail" className="w-full" />
             <p className="mt-2 font-medium">{video.snippet.title}</p>
+            
           </div>
         ))}
       </div>
